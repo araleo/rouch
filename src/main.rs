02412236a7566b84543c301dba9119cf4ac53af0
Leonardo Araujo <mendes.lnr@gmail.com>
2023-03-16 17:28:45 +0200

    Refactoring to split code into functions

--- src/main.rs
+++ src/main.rs
@@ -7,32 +7,18 @@ use std::{
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    check_args(&args);
+    let filepath = &args[1].replace('\\', "/");
+    let elements = split_filepath_and_filename(filepath);
+    let dir_path = handle_dirs(&elements[0]);
+    handle_file(&elements[1], dir_path);
+}
 
+fn check_args(args: &Vec<String>) {
     if args.len() != 2 {
         eprintln!("USAGE: rouch <path>");
         process::exit(1);
     }
-
-    let filepath = &args[1];
-    let elements = split_filepath_and_filename(filepath);
-    let dir = &elements[0];
-
-    let dir_path = Path::new(dir);
-    if !dir_path.exists() {
-        let result = create_dir_all(dir_path);
-        if let Err(e) = result {
-            eprintln!("ERROR: Could not create dir {dir_path:?}: {e}.");
-            process::exit(2);
-        };
-    }
-
-    let filename = &elements[1];
-    let filepath = dir_path.join(filename);
-    let result = File::create(&filepath);
-    if let Err(e) = result {
-        eprintln!("ERROR: Could not create file {filepath:?}: {e}.");
-        process::exit(3);
-    };
 }
 
 fn split_filepath_and_filename(filepath: &str) -> Vec<String> {
@@ -42,4 +28,33 @@ fn split_filepath_and_filename(filepath: &str) -> Vec<String> {
         return [dir.to_string(), filename.to_string()].to_vec();
     }
     [".".to_string(), filepath.to_string()].to_vec()
+}
+
+fn handle_dirs(dir: &String) -> &Path {
+    let dir_path = Path::new(dir);
+    if !dir_path.exists() {
+        create_intermediate_dirs(dir_path);
+    }
+    dir_path
+}
+
+fn create_intermediate_dirs(dir_path: &Path) {
+    let result = create_dir_all(dir_path);
+    if let Err(e) = result {
+        eprintln!("ERROR: Could not create dir {dir_path:?}: {e}.");
+        process::exit(2);
+    };
+}
+
+fn handle_file(filename: &String, dir_path: &Path) {
+    let filepath = dir_path.join(filename);
+    create_file(&filepath);
+}
+
+fn create_file(filepath: &Path) {
+    let result = File::create(filepath);
+    if let Err(e) = result {
+        eprintln!("ERROR: Could not create file {filepath:?}: {e}.");
+        process::exit(3);
+    };
 }
